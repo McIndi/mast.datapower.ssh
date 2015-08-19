617426301ae28d694725c340805b069f66834d69
ilovetux <me@ilovetux.com>
2015-08-19 09:58:18 -0400

    Initial push

---
+++ README.md
@@ -0,0 +1,2 @@
+# mast.datapower.ssh
+
---
+++ setup.py
@@ -0,0 +1,40 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "mast.datapower.ssh",
+    version = "2.0.0",
+    author = "Clifford Bressette",
+    author_email = "cliffordbressette@mcindi.com",
+    description = ("A utility to help manage users and groups for IBM DataPower"),
+    license = "GPLv3",
+    keywords = "DataPower user group account rbm",
+    url = "http://github.com/mcindi/mast.datapower.accounts",
+    namespace_packages=["mast", "mast.datapower"],
+    packages=['mast', 'mast.datapower', 'mast.datapower.ssh'],
+    entry_points = {
+        'mast_web_plugin': [
+            'ssh = mast.datapower.ssh:WebPlugin'
+        ]
+    },
+    data_files=[
+        ("mast/datapower/ssh/data", [
+            "./mast/datapower/ssh/docroot/plugin.js",
+            "./mast/datapower/ssh/docroot/plugin.css",
+            "./mast/datapower/ssh/docroot/plugin.html"
+        ])
+    ],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: GPLv3",
+    ],
+)
